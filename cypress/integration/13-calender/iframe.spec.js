63733c25708f993c3defc4114341776240b86bb8
Minskoleopc <minskoleopc@gmail.com>
2022-08-01 07:42:35 +0530

    iframe

---
+++ cypress/integration/13-calender/iframe.spec.js
@@ -0,0 +1,53 @@
+/// <reference types="cypress" />
+
+describe('verify the iframe in cypress',function(){
+
+
+    let getIframeBody = function(id){
+        return cy.get(`#${id}`)
+        .its('0.contentDocument.body').should('not.be.empty')
+        .then(cy.wrap)
+    }
+
+
+    it('verify the iframe element using javascript ',function(){
+        cy.visit('https://webdriveruniversity.com/IFrame/index.html')
+        //cy.get('a[href="index.html"]').click()
+        cy.get('#frame').then(function(iframe){
+            let body = iframe[0].contentDocument.body
+            cy.wrap(body).as('bdy')
+            cy.get('@bdy').find('a[href="index.html"]').should('have.text',"Home")
+        })
+
+    })
+
+    it.skip('verify the iframe element using jquery ',function(){
+
+        // ifram tagName -----> contents(document) ------> body ----> wrap----findElement
+        cy.visit('https://webdriveruniversity.com/IFrame/index.html')
+        cy.get('#frame').then(function($iframe){
+            let body = $iframe.contents().find('body')
+            cy.wrap(body).as('bdy')
+            cy.get('@bdy').find('a[href="index.html"]').should('have.text',"Home")
+        })
+
+    })
+
+    it.only('verify the iframe element using jquery ',function(){
+
+        // ifram tagName -----> contents(document) ------> body ----> wrap----findElement
+        cy.visit('https://webdriveruniversity.com/IFrame/index.html')
+        getIframeBody('frame').then(function(bdy){
+            cy.wrap(bdy).as('body')
+            cy.get('@body').find('a[href="index.html"]').should('have.text',"Home")
+        })
+       
+
+    })
+
+
+
+
+
+
+})
